442185da1a4395bac2649060385c185a2b0283e1
Jaime Fournier <jaimef@linbsd.org>
2016-04-28 10:50:57 -0700

    add benchmark mode

--- collector/main.lisp
+++ collector/main.lisp
@@ -28,6 +28,7 @@
       ((equal "main" verb) (main))
       ((equal "a" verb)(time (cloudtrail-report-to-psql-async workers dir)))
       ((equal "s" verb)(time (cloudtrail-report-to-psql-sync dir)))
+      ((equal "r" verb)(time (run-bench)))
       (t (progn
 	   (format t "Usage: <~A> <function> <args>~%" (nth 0 args))
 	 (format t "Function is (s) for single threaded, and (a) for multithreaded~%")
@@ -42,4 +43,25 @@
   (cond
     ((equal "s" verb) (profile (ctcl::cloudtrail-report-to-psql-sync dir)))
     ((equal "a" verb) (profile (ctcl::cloudtrail-report-to-psql-async workers dir)))
+    ((equal "r" verb)(time (run-bench)))
     (t (format t "Usage <~A> <p or s> <directory of logs>" app))))
+
+(defun run-bench () 
+  (ql:quickload :swank)
+  (load "collector/load.lisp")
+  (princ "XXX: Ensuring connections")
+  (psql-ensure-connection "metis")
+  ;;(princ "XXX: Dropping tables")
+  (create-tables-psql "metis")
+  (princ "XXX: Running Test")
+  ;;(setq swank:*use-dedicated-output-stream* nil)
+  ;;(swank:create-server :dont-close t :port 2221)
+  #+sbcl (cl-user::profile "CTCL")
+  #+allegro
+  (progn
+    ;;XX(setf excl:*tenured-bytes-limit* 524288000)
+    (prof:with-profiling (:type :time) (ctcl::cloudtrail-report-to-psql-async "10" "~/test-ct/"))
+    (prof:show-flat-profile))
+  #-allegro
+  (time (ctcl::cloudtrail-report-to-psql-async "10" "~/test-ct/"))
+  )
