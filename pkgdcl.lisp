1c2b8775523642a925a7bc805929a69fd774b7e2
Jaime Fournier <jaimef@linbsd.org>
2016-05-19 09:41:07 -0700

    remove shit

--- pkgdcl.lisp
+++ pkgdcl.lisp
@@ -1,5 +1,5 @@
 (load (merge-pathnames "~/quicklisp/setup.lisp" *default-pathname-defaults*))
-(ql:quickload '(:fare-memoization :cl-fad :gzip-stream :cl-json :s-sql :pcall :uiop :cl-store :rucksack))
+(ql:quickload '(:fare-memoization :cl-fad :gzip-stream :cl-json :s-sql :pcall :uiop :cl-store))
 (load "version.lisp")
 
 (defpackage :ctcl
