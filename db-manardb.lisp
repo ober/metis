7209729cd99c3f9ecb00f1e9799f0f2ea838ae50
Jaime Fournier <jaimef@linbsd.org>
2016-11-07 11:54:26 -0500

    stubs

---
+++ db-manardb.lisp
@@ -0,0 +1,18 @@
+(in-package :metis)
+
+
+(defun manardb-recreate-tables ()
+  (print "manardb-recreate-tables")
+  )
+
+(defun manardb-normalize-insert (record)
+  (format t "manardb-nomalize-insert ~A~%" record)
+  )
+
+(defun manardb-get-or-insert-id (table value)
+  (format t "manard-get-or-insert-id table:~A value:~A~%" table value)
+  )
+
+(defun manardb-drop-table (query)
+  (format t "manardb-drop-table query:~A~%")
+  )
