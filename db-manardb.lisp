dba9daf7a63fe2deffd5b0e8ce669269122e6f01
Jaime Fournier <jaimef@linbsd.org>
2016-11-16 01:17:54 -0500

    fix perf.

--- db-manardb.lisp
+++ db-manardb.lisp
@@ -72,27 +72,15 @@
 
 (defun get-by-name (name)
   (manardb:doclass (x 'metis::ct :fresh-instances nil)
-    (manardb:with-cached-slots (userName eventTime eventName eventSource sourceIPAddress userAgent errorMessage errorCode) x
-      (if (string-equal name userName)
-	  (format t "|~A|~A|~A|~A|~A|~A|~A|~%" eventTime eventName eventSource sourceIPAddress userAgent errorMessage errorCode)))))
-
-;; (defun get-by-name (name)
-;;   (mapcar
-;;    (lambda (x)
-;;        (print-record-a x)
-;;   	 )
-;;    (remove-if-not
-;;     (lambda (x) (string-equal name (slot-value x 'userName)))
-;;     (manardb:retrieve-all-instances 'metis::ct))))
+    (if (string-equal name (slot-value x 'userName))
+	;;(with-slots (userName eventTime eventName eventSource sourceIPAddress userAgent errorMessage errorCode) x
+	  (format t ".~%"))))
+	  ;;(format t "|~A|~A|~A|~A|~A|~A|~A|~%" eventTime eventName eventSource sourceIPAddress userAgent errorMessage errorCode)))))
 
 (defun manardb-recreate-tables ()
   (format t "manardb-recreate-tables~%"))
 
 (defun manardb-normalize-insert (record)
-  ;;(format t "manardb-normalize-insert ~A~%" record)
-  ;; (make-instance 'ct :eventTime (first record) :userName (second record))
-  ;; (print (length (manardb:retrieve-all-instances 'ct)))
-  ;; )
   ;;manardb-nomalize-insert (NIL us-west-1 NIL NIL 216e957f-230e-42ea-bfc7-e0d07d321a8b DescribeDBInstances rds.amazonaws.com 2015-08-07T19:04:52Z AwsApiCall 1.03 224108527019 2f1d4165-3d37-11e5-aae4-c1965b0823e9 NIL NIL NIL bogus.example.com signin.amazonaws.com (invokedBy signin.amazonaws.com sessionContext (attributes (creationDate 2015-08-07T11:17:07Z mfaAuthenticated true)) userName meylor accessKeyId ASIAIOHLZS2V2QON52LA accountId 224108527019 arn arn:aws:iam::224108527019:user/meylor principalId AIDAJVKKNU5BSTZIOF3EU type IAMUser) meylor)
   (destructuring-bind (
   		       additionalEventData
