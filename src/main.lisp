11a207d66f0520b0d286752f708b485fcfa58186
Jaime Fournier <jaimef@linbsd.org>
2017-01-19 23:56:18 -0500

    move to symbols

--- src/main.lisp
+++ src/main.lisp
@@ -110,6 +110,7 @@
       ((equal "vs" verb)(time (vpc-flows-report-sync alpha)))
       ((equal "vsa" verb)(time (vpc-search-by-srcaddr alpha)))
       ((equal "vsp" verb)(time (vpc-search-by-srcport alpha)))
+      ((equal "ctf" verb)(time (get-ct-files)))
       (t (progn
 	   (usage))))
   (manardb:close-all-mmaps)))
