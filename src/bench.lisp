1aae8957f71654d0ec43d8495d5d5450386adbf7
Jaime Fournier <jaimef@linbsd.org>
2017-01-19 23:32:38 -0500

    fix bench stuff

--- src/bench.lisp
+++ src/bench.lisp
@@ -1,35 +1,37 @@
+;;(ql:quickload :metis)
 (in-package :metis)
 
+(defun init-manardb-bench()
+  (if (and (eql (hash-table-count *manard-files*) 0) *metis-need-files*)
+      (allocate-file-hash)))
+
 (defun do-bench ()
-  (setf *DB* "metis")
+  (defvar *db-backend* :manardb)
+  (init-manardb-bench)
+  (manardb:use-mmap-dir "~/bench-mdb/")
   (declaim (optimize (safety 3) (speed 0) (debug 3)))
   (defvar *benching* t)
-  ;;(defvar *db-backend* :manardb)
   ;;(cloudtrail-report-async "1" "~/nov/"))
-  (cloudtrail-report-sync "~/nov/"))
+  (cloudtrail-report-sync "~/ct-test/"))
 
 (defun run-bench ()
-  (sqlite-establish-connection)
-  (defvar database "metis")
-  (db-recreate-tables "metis")
   (princ "XXX: Running Test")
-  ;;#+sbcl (time (do-bench))
-  ;; (progn
-  ;; 	(sb-sprof:with-profiling (:report :flat) (do-bench)))
+  #+sbcl ;;(time (do-bench))
+  (progn
+    (sb-sprof:with-profiling (:report :flat) (do-bench)))
   #+lispworks  ;;(hcl:extended-time (do-bench))
   (progn
     (hcl:set-up-profiler :package '(metis))
     (hcl:profile (do-bench)))
-    ;;  #+allegro
-    ;;(progn
-    ;;(setf excl:*tenured-bytes-limit* 524288000)
-    ;;(prof::with-profiling (:type :space) (ctcl::do-bench))
-    ;;(prof::show-flat-profile))
-  #+(or clozure abcl ecl) (time (do-bench))
-    (time (do-bench))
-    ;;(format t "results: size:~A" (queue-length *q*))
-    ;;(cl-store:store *q* "~/q.store")
+  #+allegro
+  (progn
+    (setf excl:*tenured-bytes-limit* 524288000)
+    (setf *maxsamples* 100)
+    (prof::with-profiling (:type :time) (metis::do-bench))
+    (prof::show-call-graph)
+    (prof::show-flat-profile))
 
-    )
+  #+(or clozure abcl ecl) (time (do-bench))
+  )
 
 ;;(run-bench)
