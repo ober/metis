8c0516cd18c4194d7b07709ebe000fb144e13abf
Jaime Fournier <jaimef@linbsd.org>
2017-02-03 22:37:26 -0500

    nol love

--- src/deliver-ecl.lisp
+++ src/deliver-ecl.lisp
@@ -11,6 +11,28 @@
       asdf:*central-registry*)
 
 (setf *compile-print* nil)
+(ql:bundle-systems '(
+	       :cffi-grovel
+	       :cl-date-time-parser
+	       :cl-fad
+	       :cl-json
+	       :closer-mop
+	       :fare-memoization
+	       :gzip-stream
+	       :jonathan
+	       :local-time
+	       :manardb
+	       :pcall
+	       :pcall-queue
+	       :split-sequence
+	       :thnappy
+	       :trivial-garbage
+	       :uiop
+	       :usocket
+	       :zs3
+		     ) :to "bundle/")
+
+(load "bundle/bundle.lisp")
 (asdf:operate 'asdf:load-op 'metis)
 ;;(asdf:operate 'asdf:load-op 'closer-mop)
 
@@ -18,5 +40,5 @@
 ;;(asdf:operate 'asdf:load-bundle-op 'metis)
 ;;(ql:quickload :closer-mop)
 
-(asdf:make-build :metis :type :program :prologue-code '(require :asdf) :epilogue-code '(metis::main) :move-here "./")
+(asdf:make-build :metis :type :program :prologue-code '(progn (require :asdf)(require :uiop)) :epilogue-code '(metis::main) :move-here "./")
 ;;(asdf:make :metis :type :program :epilogue-code 'metis:main)
