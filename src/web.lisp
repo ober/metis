7cb083a3cc94dc0fa94f66bba69a7e2e5849dc10
Jaime Fournier <jaimef@linbsd.org>
2017-03-02 13:42:59 -0500

    add working web stuff

--- src/web.lisp
+++ src/web.lisp
@@ -4,6 +4,21 @@
 (metis::init-manardb)
 
 (setf (ningle:route *app* "/") "Welcome to metis")
-(setf (ningle:route *app* "/users") `(200 (:content-type "text/html") ,(metis::get-unique-values-list-html 'metis::username)))
+
+
+(defun web/get-user-list ()
+  (let ((users (metis::get-unique-values-list 'metis::username)))
+    (format nil "~{<li><a href=\"http://localhost:5002/user-search?user=~A\"> ~:* ~A </aref></li>~}" users users)))
+
+(defun web/search-user (user)
+  (let* ((activity (metis::ct-get-by-klass-value-real 'metis::userName user))
+    (format nil "~{<li>~A</li>~}" activity))))
+
+(setf (ningle:route *app* "/users") `(200 (:content-type "text/html") (,(web/get-user-list))))
+
+(setf (ningle:route *app* "/user-search")
+      #'(lambda (params)
+	  (let ((user (cdr (assoc "user" params :test #'string=))))
+	    `(200 (:content-type "text/html") (,(web/search-user user))))))
 
 (clack:clackup *app* :port 5002 :server :hunchentoot)
