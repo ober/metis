d30c4898deb14823f3b7ed5a7d6d3ec6809e8bcb
Jaime Fournier <jaimef@linbsd.org>
2017-04-14 14:47:40 -0700

    add ops

--- chicken/metis.scm
+++ chicken/metis.scm
@@ -152,20 +152,9 @@
 
 (lmdb-close *db*)
 
-(use args)
-
 (define opts
- (list (args:make-option (c cookie)    #:none     "give me cookie"
-         (print "cookie was tasty"))
-       (args:make-option (d)           (optional: "LEVEL")  "debug level [default: 1]"
-         (set! arg (string->number (or arg "1"))))
-       (args:make-option (e elephant)  #:required "flatten the argument"
-         (print "elephant: arg is " arg))
-       (args:make-option (f file)      (required: "NAME")   "parse file NAME")
-       (args:make-option (v V version) #:none     "Display version"
-         (print "args-example $Revision: 1.3 $")
-         (exit))
-       (args:make-option (abc)         #:none     "Recite the alphabet")
+ (list (args:make-option (l load) (required: "DIR")  "Load Cloudtrail files in directory"
+			 (ct-report-dir arg))
        (args:make-option (h help)      #:none     "Display this text"
          (usage))))
 
@@ -175,9 +164,8 @@
      (print "Usage: " (car (argv)) " [options...] [files...]")
      (newline)
      (print (args:usage opts))
-     (print "Report bugs to zbigniewsz at gmail.")))
+     (print "Report bugs to ober at linbsd.org")))
  (exit 1))
 
 (receive (options operands)
-    (args:parse (command-line-arguments) opts)
-  (print "-e -> " (alist-ref 'elephant options))) ;; 'e or 'elephant both work
+    (args:parse (command-line-arguments) opts))
