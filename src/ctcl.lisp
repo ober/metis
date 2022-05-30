162a9a5711fd924c1e0105b995e84e3b2ab7339f
ober <jaimef@linbsd.org>
2022-05-30 15:30:43 -0600

    fix up

--- src/ctcl.lisp
+++ src/ctcl.lisp
@@ -10,8 +10,8 @@
 (defun have-we-seen-this-file (file)
   (let ((found (db-have-we-seen-this-file file)))
     (if found
-	t
-	nil)))
+        t
+        nil)))
 
 (defun walk-ct (path fn)
   (cl-fad:walk-directory path fn))
@@ -21,15 +21,15 @@
 
 (defun async-ct-file (x)
   (push (pcall:pexec
-	    (funcall #'process-ct-file x)) *mytasks*))
+         (funcall #'process-ct-file x)) *mytasks*))
 
 (defun process-ct-file (x)
   "Handle the contents of the json gzip file"
   (when (equal (pathname-type x) "gz")
     (unless (db-have-we-seen-this-file x)
       (progn
-	(db-mark-file-processed x)
-	(parse-ct-contents x)))))
+        (db-mark-file-processed x)
+        (parse-ct-contents x)))))
 
 (defun fetch-value (indicators plist)
   "Return the value at the end of the indicators list"
@@ -37,23 +37,23 @@
 
 (defun get-filename-hash (file)
   (car (cl-ppcre:split #\.
-		       (nth 4 (cl-ppcre:split "_" (file-namestring file))))))
+                       (nth 4 (cl-ppcre:split "_" (file-namestring file))))))
 
 (defun parse-ct-contents (x)
   "process the json output"
   (handler-case
       (progn
-	(let* ((records (second (read-json-gzip-file x)))
-	       (num (length records))
-	       (btime (get-internal-real-time)))
-	  (dolist (x records)
-	    (normalize-insert (process-record x *fields*)))
-	  (let* ((etime (get-internal-real-time))
-		 (delta (/ (float (- etime btime)) (float internal-time-units-per-second)))
-		 (rps (ignore-errors (/ (float num) (float delta)))))
-	    (if (> num 100)
-		(format t "~%rps:~A rows:~A delta:~A" rps num delta))
-	    )))
+        (let* ((records (second (read-json-gzip-file x)))
+               (num (length records))
+               (btime (get-internal-real-time)))
+          (dolist (x records)
+            (normalize-insert (process-record x *fields*)))
+          (let* ((etime (get-internal-real-time))
+                 (delta (/ (float (- etime btime)) (float internal-time-units-per-second)))
+                 (rps (ignore-errors (/ (float num) (float delta)))))
+            (if (> num 100)
+                (format t "~%rps:~A rows:~A delta:~A" rps num delta))
+            )))
     (t (e) (error-print "read-json-gzip-file" e)))
   ;;#+sbcl (room) ;; (trivial-garbage:gc)
   )
@@ -61,23 +61,24 @@
 (defun cloudtrail-report-sync (path)
   (setf *metis-need-files* t)
   (init-manardb)
-  (init-ct-hashes)
+  ;;(init-ct-hashes)
   (force-output)
   (let ((cloudtrail-reports (or path "~/CT")))
     (walk-ct cloudtrail-reports
-	     #'sync-ct-file)))
+             #'sync-ct-file)))
 
 (defun cloudtrail-report-async (workers path)
   (setf *metis-need-files* t)
-  (init-manardb)
-  (init-ct-hashes)
+  (time (init-manardb))
+  ;;(time (init-ct-hashes))
   (force-output)
   (let ((workers (parse-integer workers)))
     (setf (pcall:thread-pool-size) workers)
     (let ((cloudtrail-reports (or path "~/CT")))
       (walk-ct cloudtrail-reports
-	       #'async-ct-file))
-    (mapc #'pcall:join *mytasks*)))
+               #'async-ct-file))
+    (mapc #'pcall:join *mytasks*))
+  (myexit))
 
 (defun sync-from-s3 (bucket)
   ;;(setf zs3::*credentials* (zs3:file-credentials "~/.aws/s3.conf"))
@@ -86,7 +87,7 @@
       ((exists (bucket-exists-p bucket)))
     (format t "bucket:~A exists:~A~%" bucket exists)
     (mapc #'(lambda (x)
-	      (format t "~A~%"
-		      (get-filename-hash
-		       (slot-value x 'zs3:name))))
-	  (coerce (all-keys bucket) 'list))))
+              (format t "~A~%"
+                      (get-filename-hash
+                       (slot-value x 'zs3:name))))
+          (coerce (all-keys bucket) 'list))))
