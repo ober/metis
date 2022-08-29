932b9bc51b91fa661af319aa2fd43ded1a472e20
jafourni <jaimef@linbsd.org>
2022-08-29 14:40:12 -0600

    disable ip lookup. sbcl chokes

--- src/utils.lisp
+++ src/utils.lisp
@@ -1,25 +1,28 @@
 (in-package :metis)
 
 (fare-memoization:define-memo-function get-hostname-by-ip (ip)
-  (if (boundp '*benching*)
-      "bogus.example.com"
-      (progn
-	(if (cl-ppcre:all-matches "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" ip)
-	    (let ((name
-		   #+allegro
-		    (ignore-errors (socket:ipaddr-to-hostname ip))
-		    #+sbcl
-		    (ignore-errors (sb-bsd-sockets:host-ent-name
-				    (sb-bsd-sockets:get-host-by-address
-				     (sb-bsd-sockets:make-inet-address ip))))
-		    #+lispworks
-		    (ignore-errors (comm:get-host-entry ip :fields '(:name)))
-		    #+clozure
-		    (ignore-errors (ccl:ipaddr-to-hostname (ccl:dotted-to-ipaddr ip)))))
-	      (if (null name)
-		  ip
-		  name))
-	    ip))))
+  (handler-case
+      (if (boundp '*benching*)
+          "bogus.example.com"
+          (progn
+            (if (cl-ppcre:all-matches "^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$" ip)
+                (let ((name
+                        #+allegro
+                        (ignore-errors (socket:ipaddr-to-hostname ip))
+                        #+sbcl
+                        (ignore-errors
+                         (sb-bsd-sockets:host-ent-name
+                          (sb-bsd-sockets:get-host-by-address
+                           (sb-bsd-sockets:make-inet-address ip))))
+                        #+lispworks
+                        (ignore-errors (comm:get-host-entry ip :fields '(:name)))
+                        #+clozure
+                        (ignore-errors (ccl:ipaddr-to-hostname (ccl:dotted-to-ipaddr ip)))))
+                  (if (null name)
+                      ip
+                      name))
+                ip)))
+    (t (e) (error-print "get-hostname-by-ip" e))))
 
 (defun read-json-gzip-file (file)
   (handler-case
