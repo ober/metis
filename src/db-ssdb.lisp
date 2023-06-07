4f068c1a0a1b9ba8d6d9b70e2c1ea74e4e339c04
jafourni <jaimef@linbsd.org>
2023-06-06 20:30:51 -0600

    updates

--- src/db-ssdb.lisp
+++ src/db-ssdb.lisp
@@ -3,17 +3,16 @@
 ;; fundamental metis ops
 (defun ssdb/init ()
   (unless ssdb:*connection*
-    (ssdb:connect)))
+    (ssdb:connect :host "0.0.0.0" :port 8888)))
 
 (defun ssdb/close ()
-  (ssdb:flushdb)
   (ssdb:disconnect))
 
 (defun ssdb/have-we-seen-this-file (file)
-  (ssdb:exists (format nil "F-~a" file)))
+  (ssdb:exists (format nil "F-~a" (get-filename-hash file))))
 
 (defun ssdb/mark-file-processed (file)
-  (ssdb:set (format nil "F-~a" file) "1"))
+  (ssdb:set (format nil "F-~a" (get-filename-hash file)) "1"))
 
 (defun ssdb/normalize-insert (record)
   (destructuring-bind (
@@ -47,67 +46,51 @@
                        vpcEndpointId
                        )
       record
-    (format t "userAgent~a~%" userAgent)))
-    ;; (let ((additionalEventData-i (get-idx 'metis::additionalEventData additionalEventData))
-    ;;       (apiVersion-i (get-idx 'metis::apiVersion apiVersion))
-    ;;       (awsRegion-i (get-idx 'metis::awsRegion awsRegion))
-    ;;       (errorCode-i (get-idx 'metis::errorCode errorCode))
-    ;;       (errorMessage-i (get-idx 'metis::errorMessage errorMessage))
-    ;;       (eventCategory-i (get-idx 'metis::eventCategory eventCategory ))
-    ;;       (eventID-i (get-idx 'metis::eventID eventID))
-    ;;       (eventName-i (get-idx 'metis::eventName eventName))
-    ;;       (eventSource-i (get-idx 'metis::eventSource eventSource))
-    ;;       (eventTime-i (get-idx 'metis::eventTime eventTime))
-    ;;       (eventType-i (get-idx 'metis::eventType eventType))
-    ;;       (eventVersion-i (get-idx 'metis::eventVersion eventVersion))
-    ;;       (managementEvent-i (get-idx 'metis::managementEvent managementEvent))
-    ;;       (readOnly-i (get-idx 'metis::readOnly  readOnly))
-    ;;       (recipientAccountId-i (get-idx 'metis::recipientAccountId recipientAccountId))
-    ;;       (requestID-i (get-idx 'metis::requestID requestID))
-    ;;       (requestParameters-i (get-idx 'metis::requestParameters requestParameters))
-    ;;       (resources-i (get-idx 'metis::resources resources))
-    ;;       (responseElements-i (get-idx 'metis::responseElements  responseElements))
-    ;;       (serviceEventDetails-i (get-idx 'metis::serviceEventDetails serviceEventDetails))
-    ;;       (sessionCredentialFromConsole-i (get-idx 'metis::sessionCredentialFromConsole sessionCredentialFromConsole))
-    ;;       (sharedEventID-i (get-idx 'metis::sharedEventID sharedEventID))
-    ;;       (sourceIPAddress-i (get-idx 'metis::sourceIPAddress sourceIPAddress))
-    ;;       (tlsDetails-i (get-idx 'metis::tlsDetails tlsDetails))
-    ;;       (userAgent-i (get-idx 'metis::userAgent userAgent))
-    ;;       (userIdentity-i (get-idx 'metis::userIdentity userIdentity))
-    ;;       (userName-i (get-idx 'metis::userName (or userName (find-username userIdentity))))
-    ;;       (vpcEndpointId-i (get-idx 'metis::vpcEndpointId vpcEndpointId)))
-    ;;   (make-instance 'ct
-    ;;                  :additionalEventData additionalEventData-i
-    ;;                  :apiVersion apiVersion-i
-    ;;                  :awsRegion awsRegion-i
-    ;;                  :errorCode errorCode-i
-    ;;                  :errorMessage errorMessage-i
-    ;;                  :eventCategory eventCategory-i
-    ;;                  :eventID eventID-i
-    ;;                  :eventName eventName-i
-    ;;                  :eventSource eventSource-i
-    ;;                  :eventTime eventTime-i
-    ;;                  :eventType eventType-i
-    ;;                  :eventVersion eventVersion-i
-    ;;                  :managementEvent managementEvent-i
-    ;;                  :readOnly readOnly-i
-    ;;                  :recipientAccountId recipientAccountId-i
-    ;;                  :requestID requestID-i
-    ;;                  :requestParameters requestParameters-i
-    ;;                  :resources resources-i
-    ;;                  :responseElements responseElements-i
-    ;;                  :serviceEventDetails serviceEventDetails-i
-    ;;                  :sessionCredentialFromConsole sessionCredentialFromConsole-i
-    ;;                  :sharedEventID sharedEventID-i
-    ;;                  :sourceIPAddress sourceIPAddress-i
-    ;;                  :tlsDetails tlsDetails-i
-    ;;                  :userAgent userAgent-i
-    ;;                  :userIdentity userIdentity-i
-    ;;                  :userName userName-i
-    ;;                  :vpcEndpointId vpcEndpointId-i
+    (let* ((epoch (rfc3339-to-epoch eventTime))
+           (handle (format nil "~a:~a" epoch requestID)))
+      (ssdb:multi_hset handle
+                       "aed" additionalEventData
+                       "av" apiVersion
+                       "ar" awsRegion
+                       "ec" errorCode
+                       "em" errorMessage
+                       "eca" eventCategory
+                       "en" eventName
+                       "es" eventSource
+                       ;;"ei" eventID
+                       ;;"et" eventTime
+                       "typ" eventType
+                       "ev" eventVersion
+                       "me" managementEvent
+                       "ro" readOnly
+                       "rai" recipientAccountId
+                       "rqi" requestID
+                       "rp" requestParameters
+                       "res" resources
+                       "re" responseElements
+                       "sed" serviceEventDetails
+                       "scf" sessionCredentialFromConsole
+                       "sei" sharedEventID
+                       "sia" sourceIPAddress
+                       "td" tlsDetails
+                       "ua" userAgent
+                       "ui" userIdentity
+                       "un" userName
+                       "vpc" vpcEndpointId)
 
+      (ssdb:qpush userName handle)
+      (ssdb:qpush eventName handle)
+      (ssdb:qpush errorCode handle)
+      )))
 
 ;; ported kunabi style ops
+
+(defun ssdb/fetch-print-hash (hit)
+  (format t "|~a| ~{~a: ~a| ~}|~%"
+          (epoch-to-rfc3339
+           (parse-number:parse-number
+            (car (cl-ppcre:split ":" hit))))
+          (ssdb:multi_hget hit "rqi" "en" "un" "ui" "ua" "sia" "ec" "em" "sip" "ua" "es" "res" "rp")))
 
 (defun ssdb/db-key? (key)
   (ssdb:exists key))
@@ -115,4 +98,119 @@
 (defun ssdb/db-get (key)
   (ssdb:get key))
 
-;; kv related
+(defun ssdb/get-stats ()
+  (format t "~a" (ssdb:info)))
+
+(defun ssdb/get-unique-region ()
+  (ssdb/get-unique "ar"))
+
+(defun ssdb/get-unique-ip ()
+  (ssdb/get-unique "sia"))
+
+(defun ssdb/get-unique-agent ()
+  (ssdb/get-unique "ua"))
+
+(defun ssdb/get-unique-names ()
+  (ssdb/get-unique "un"))
+
+(defun ssdb/get-unique-events ()
+  (ssdb/get-unique "en"))
+
+(defun ssdb/get-unique-errorcode ()
+  (ssdb/get-unique "ec"))
+
+(defun ssdb/get-unique (key)
+  (let ((hits (sort-uniq (ssdb:qrange key 0 -1))))
+    (mapcar
+     (lambda (hit)
+       (format t "~a~%" hit))
+     hits)))
+
+(defun ssdb/count-errors ()
+  (let* ((from (format nil "~a:" (epoch-one-day-ago)))
+         (to (format nil "~a:" (epoch-now)))
+         (records (ssdb:hlist from to -1)))
+    (format t "records: ~a~%" (length records))
+    (mapcar
+     (lambda (record)
+       (let ((un (ssdb:hget record "un"))
+             (ec (ssdb:hget record "ec")))
+         (ssdb:zincr un ec 1)))
+     records)))
+
+(defun ssdb/count-calls ()
+  (let* ((from (format nil "~a:" (epoch-one-day-ago)))
+         (to (format nil "~a:" (epoch-now)))
+         (records (ssdb:hlist from to -1))
+         (seen '()))
+    (format t "records: ~a~%" (length records))
+    (mapcar
+     (lambda (record)
+       (destructuring-bind (_ un _ ec _ sia _ en) (ssdb:multi_hget record "un" "ec" "sia" "en")
+         (unless (member un seen :test #'string=)
+           (progn
+             (push un seen)
+             (format t "Not seen ~a~%" un)
+             (ssdb:zclear un)))
+         (ssdb:zincr un en 1)
+         (ssdb:zincr un ec 1)
+         (ssdb:zincr un sia 1)))
+     records)))
+
+(defun ssdb/count-by-user (user)
+  (let ((ops (ssdb:zkeys user "" "" "" -1)))
+    (format t "Total: ~a~%" (ssdb:zsum user "" ""))
+    (mapcar
+     (lambda (op)
+       (format t "|~a| ~a|~%" op (ssdb:zget user op)))
+     ops)))
+
+(defun ssdb/totals ()
+  (let ((users (ssdb:zlist "" "" -1)))
+    (mapcar
+     (lambda (user)
+       (format t "| ~a | ~a |~%" user (ssdb:zsum user "" "")))
+     users)))
+
+(defun ssdb/index (field)
+  (let* ((from (format nil "~a:" (epoch-one-day-ago)))
+         (to (format nil "~a:" (epoch-now)))
+         (records (ssdb:hlist from to -1))
+         (seen '()))
+    ;;(ssdb:qclear field)
+    (format t "records: ~a~%" (length records))
+    (mapcar
+     (lambda (record)
+       (unless (string= record "NIL")
+         (let ((value (ssdb:hget record field)))
+           (ssdb:qpush value record)
+           (unless (member value seen :test #'string=)
+             (progn
+               (format t "not seen: ~a~%" value)
+               (push value seen)
+               (ssdb:qpush field value))))))
+     records)))
+
+(defun ssdb/qpush-list (field list)
+  (mapcar
+   (lambda (item)
+     (ssdb:qpush field item))
+   (sort-uniq list)))
+
+(defun ssdb/get-by-index (key)
+  "Get all records from index of key"
+  (let ((hits (sort-uniq (ssdb:qrange key 0 -1))))
+    (mapcar
+     (lambda (hit)
+       (ssdb/fetch-print-hash hit))
+     hits)))
+
+(defun ssdb/unique-queue (q)
+  (let* ((items (ssdb:qrange q 0 -1))
+         (uniqs (sort-uniq items)))
+    (format t "q: ~a orig: ~a uniq: ~a~%" q (length items) (length uniqs))
+    (ssdb:qclear q)
+    (mapcar
+     (lambda (uniq)
+       (ssdb:qpush q uniq))
+     uniqs)))
